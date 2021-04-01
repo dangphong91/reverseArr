643edfb9c8966b55886c381efb7f041495189303
dphong08 <dphongg199x@gmail.com>
2021-04-01 09:16:17 +0700

    up

---
+++ src/Arr.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Arr {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size does not exceed 20");
+        } while (size > 20);
+        int[] arr = new int[size];
+        int i = 0;
+        while (i < arr.length) {
+            arr[i] = (int) (Math.random()*30);
+            i++;
+        }
+        System.out.printf("%-20s%s", "Elements in array: ", "");
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[j] + "\t");
+        }
+        System.out.println();
+        for (int j = 0; j < arr.length / 2; j++) {
+            int temp = arr[j];
+            arr[j] = arr[size - 1 - j];
+            arr[size - 1 - j] = temp;
+        }
+        System.out.printf("%-20s%s", "Reverse array: ", "");
+        for (int j = 0; j < arr.length; j++) {
+            System.out.print(arr[j] + "\t");
+        }
+    }
+}
